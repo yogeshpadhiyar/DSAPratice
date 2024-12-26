3cda79d67e50605978e6322d2fcf22d6bb287e00
yogeshpadhiyar <ypadhiyar143@gmail.com>
2024-12-26 23:15:23 +0530

    add 2nd approach

--- src/main/java/com/padhiyar/dsapratice/leetcode/Leetcode494.java
+++ src/main/java/com/padhiyar/dsapratice/leetcode/Leetcode494.java
@@ -1,12 +1,14 @@
 package com.padhiyar.dsapratice.leetcode;
 
+import java.util.Arrays;
+
 public class Leetcode494 {
     int count = 0;
     public static void main(String[] args) {
         Leetcode494 leetcode494 = new Leetcode494();
         int[] nums = {1};
-        int target = 1;
-        System.out.println(leetcode494.findTargetSumWays(nums, target));
+        int target = -1;
+        System.out.println(leetcode494.findTargetSumWaysUsingDP(nums, target));
     }
 
     public int findTargetSumWays(int[] nums, int target) {
@@ -24,5 +26,27 @@ public class Leetcode494 {
         count = btTargetSum(nums, target, index+1, sum+nums[index], count);
         count = btTargetSum(nums, target, index+1, sum-nums[index], count);
         return count;
+    }
+
+    public int findTargetSumWaysUsingDP(int[] nums, int target) {
+        int[][] dp = new int[nums.length][3003];
+        for(int[] d : dp){
+            Arrays.fill(d, -1);
+        }
+        return dpTargetSum(nums, target, 0, dp);
+    }
+
+    // use 1000 plus to avoid negative index
+    private int dpTargetSum(int[] nums, int target, int i, int[][] dp) {
+        if(i == nums.length) {
+            return target == 0 ? 1 : 0;
+        }
+        if(dp[i][target+1000] != -1) {
+            return dp[i][target+1000];
+        }
+        int ans = 0;
+        ans += dpTargetSum(nums, target + nums[i], i+1, dp);
+        ans += dpTargetSum(nums, target - nums[i], i+1, dp);
+        return dp[i][target+1000] = ans;
     }
 }
